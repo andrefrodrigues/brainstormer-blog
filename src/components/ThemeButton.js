52459682bb10e1f50864b662782383bee23e14c3
André Rodrigues <andrefrodrigues21@gmail.com>
2022-02-21 21:39:15 +0000

    Add toggle button

--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+import Switch from "react-switch";
+import { SunIcon } from './sun';
+import { MoonIcon } from './moon';
 
+const wrapperStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%'
+};
 
+const iconStyle = {
+    color: 'white',
+    width: '20px',
+    height: '20px'
+}
 export function ThemeButton() {
     return (
         <ThemeToggler>
             {({ theme, toggleTheme }) => (
-                <label>
-                    <input
-                        type="checkbox"
-                        onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+                <label className="theme-button">
+                    <Switch onChange={checked => toggleTheme(checked ? 'dark' : 'light')}
                         checked={theme === 'dark'}
-                    />{' '}
-                    Dark mode
+                        checkedIcon={
+                            <span style={wrapperStyle}>
+                                <MoonIcon style={iconStyle}/>
+                            </span>
+                    }
+                        uncheckedIcon={
+                            <span style={wrapperStyle}>
+                                <SunIcon style={iconStyle}/>
+                            </span>
+                    }
+                        onColor="#18181B"
+                        offColor='#18181B'
+                    />
                 </label>
             )}
         </ThemeToggler>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
+import { ThemeButton } from "./ThemeButton"
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
@@ -22,7 +22,7 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+      <header className="global-header">{header}<ThemeButton /></header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
